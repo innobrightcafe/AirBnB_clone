eaef01159d8974582ec682f83ef4b66a9d27c874
Farad078 <faradokorie@gmail.com>
2023-02-08 21:18:54 +0100

    base_model completed

--- models/base_model.py
+++ models/base_model.py
@@ -8,34 +8,24 @@ import time
 """create a class Base"""
 class BaseModel:
     def __init__(self):
+        self.my_number = ""
+        self.name = ""
         self.id = str(uuid.uuid4())
-        self.name = None
-        self.my_number = None
-        self.created_at = datetime.datetime.now()
-        self.updated_at = None
-        self.dict = {}
+        self.created_at = str(datetime.datetime.now())
+        self.updated_at = ""
     
     def __str__(self):
-        return f"{type(self.id)} {self.id} {self.dict}"
+        return f"{[self.__class__.__name__]} ({self.id}) {self.__dict__}"
     
     def save(self):
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = str(datetime.datetime.now())
     
     def to_dict(self):
-        self.dict["my_number"] = self.my_number
-        self.dict["name"] = self.name
-        self.dict["updated_at"] = self.updated_at
-        self.dict["id"] = self.id
-        self.dict["created_at"] = self.created_at
-        return self.dict
+        self.__dict__["my_number"] = self.my_number
+        self.__dict__["name"] = self.name
+        self.__dict__["__class__"] = self.__class__.__name__
+        self.__dict__["updated_at"] = self.updated_at
+        self.__dict__["id"] = self.id
+        self.__dict__["created_at"] = self.created_at
+        return self.__dict__
 
-
-for user in range(1):
-    user = BaseModel()
-    user.name = "My First Model"
-    user.my_number = 89
-    x = user.to_dict()
-    print(user)
-    print("======================================================")
-    print(x)
-    print("======================================================")
