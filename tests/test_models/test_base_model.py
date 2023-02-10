cd8bd9790f997654ca3f45e7d9ed334b94cc76d4
macbook <macbook@inosoft-MacBook-Pro.local>
2023-02-10 08:51:43 +0100

    The airbnb clone project

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python3
 """ Module for test Base class """
+
 import unittest
-from models.base_model import Base_model
-from io import StringIO
-from unittest import TestCase
-from unittest.mock import patch
-
-
-class TestBaseMethods(unittest.TestCase):
+import datetime
+import models.base_model
+class TestBaseModel(unittest.TestCase):
     """ Suite to test Base class """
 
-    
+    def test_id(self):
+        self.assertTrue(self.base_model.id, str)
+        self.assertNotEqual(self.base_model.id, "")
+    def test_created_at(self):
+        self.assertTrue(self.base_model.created_at, datetime.datetime)
+
+    def test_updated_at(self):
+        self.assertTrue(self.base_model.updated_at, datetime.datetime)
+
+    def test_save(self):
+        before = self.base_model.updated_at
+        self.base_model.save()
+        self.assertNotEqual(before, self.base_model.updated_at)
+
+    def test_to_dict(self):
+        result = self.base_model.to_dict()
+        self.assertIsInstance(result["updated_at"], str)
+        self.assertEqual(result["__class__"], "BaseModel")
+        self.assertEqual(type(result), dict)
+
+if __name__ == "__main__":
+    unittest.main()
