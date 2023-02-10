01e8c453dd41302bd468366b472bff802d298ad9
Farad078 <faradokorie@gmail.com>
2023-02-10 13:28:52 +0100

    new update

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -4,20 +4,20 @@
 import unittest
 import sys
 
-sys.path.append('../models')
-from base_model import BaseModel
+sys.path.append('../../models')
+import base_model
 class TestBaseModel(unittest.TestCase):
     """ Suite to test Base class """
 
     def test_save(self):
-        x = BaseModel()
+        x = base_model.BaseModel()
         before = x.created_at
         x.save()
         after = x.updated_at
         self.assertNotEqual(before, after)
 
     def test_to_dict(self):
-        x = BaseModel()
+        x = base_model.BaseModel()
         x.to_dict()
         self.assertIsInstance(x.__dict__["my_number"], str)
         self.assertEqual(x.__dict__["__class__"], "BaseModel")
