2f60b2c065c57599965064506a199fb5017397ef
Fara078 <faradokorie@gmail.com>
2023-02-12 15:59:08 +0100

    base_model - pycodestyle is happy

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,8 @@ import uuid
 import datetime
 
 """create a class BaseModel"""
+
+
 class BaseModel:
     def __init__(self, *args, **kwargs):
         self.data = kwargs
@@ -13,13 +15,13 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = ""
-    
+
     def __str__(self):
         return f"{[self.__class__.__name__]} ({self.id}) {self.__dict__}"
-    
+
     def save(self):
         self.updated_at = str(datetime.datetime.now())
-    
+
     def to_dict(self):
         self.__dict__["my_number"] = self.my_number
         self.__dict__["name"] = self.name
