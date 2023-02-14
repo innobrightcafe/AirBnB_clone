85adc1edd3cad8c24d1583fe97042cc26c682d10
Farad078 <faradokorie@gmail.com>
2023-02-14 04:01:35 +0100

    new update

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-from engine.file_storage import FileStorage
+import sys
+import os
 
+dir = os.getcwd()
+folder = 'engine'
+_path = os.path.join(dir, folder)
+sys.path.append(_path)
+
+from engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
+x = 4
